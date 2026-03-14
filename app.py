579be24b8710ad34e4d3b6deb5ad77c62efbb877
mdnbabu <mdnbabu@yahoo.com>
2026-03-14 08:17:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -188,10 +188,11 @@ def keep_alive():
         except:
             pass
 
+thread = threading.Thread(target=keep_alive)
+thread.daemon = True
+thread.start()
+
 if __name__ == "__main__":
-    thread = threading.Thread(target=keep_alive)
-    thread.daemon = True
-    thread.start()
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-```
+        
